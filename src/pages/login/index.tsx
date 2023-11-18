d11285e00f0f62595d43ad35d35334cde0264bc3
HieuLe <lenhathieu96@gmail.com>
2023-11-18 16:48:58 +0700

    feat: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,9 @@ import './styles.scss'
 
 export default function LoginPage() {
   return (
-    <div className="root-container">LoginPage</div>
+    <div className="root-container">
+      <p>this is the login page</p>
+    </div>
+    
   )
 }
