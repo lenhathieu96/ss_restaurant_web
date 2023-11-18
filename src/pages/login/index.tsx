0b0ed02c317847a6f4faf8252e0e3b6d41144791
HieuLe <lenhathieu96@gmail.com>
2023-11-18 23:09:31 +0700

    feat: integrate login screen

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,6 +4,9 @@ const LoginPage = () => {
   return (
     <div className="root-container">
       <p>this is the login page demo 3</p>
+      <p>this is the login page demo 3</p>
+      <p>this is the login page demo 3</p>
+      <p>this is the login page demo 3</p>
     </div>
   );
 };
