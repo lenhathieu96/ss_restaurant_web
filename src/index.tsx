0265e9c4230e34b83addfce8921ef100b0291253
HieuLe <lenhathieu96@gmail.com>
2023-11-18 21:26:43 +0700

    feat: update login screen

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import App from './App.tsx'
 
 import './index.scss'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App/>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import './styles.scss'
 export default function LoginPage() {
   return (
     <div className="root-container">
-      <p>this is the login page</p>
+      <p>this is the login page demo</p>
     </div>
     
   )
