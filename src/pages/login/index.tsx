30a4d488bd2fa178013cbc72b5f8a74d3f6115da
HieuLe <lenhathieu96@gmail.com>
2023-11-18 22:54:20 +0700

    feat: integrate login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,7 +3,7 @@ import "./styles.scss";
 const LoginPage = () => {
   return (
     <div className="root-container">
-      <p>this is the login page demo</p>
+      <p>this is the login page demo 2</p>
     </div>
   );
 };
